ff10ddc5df7a0a1c98d6ce7148da16c7a84a4b65
NinjaMatta <97680746+NinjaMatta@users.noreply.github.com>
2022-05-09 13:00:37 +0200

    Add files via upload

---
+++ myEEPROM.h
@@ -0,0 +1,28 @@
+﻿/*
+ * myEEPROM.h
+ *
+ * Created: 09.05.2022 11:50:06
+ *  Author: Mathias Jørgensen
+ */ 
+
+#include <stdio.h>
+#include <avr/io.h>
+#include <avr/cpufunc.h>
+#include <avr/eeprom.h>
+#define F_CPU 16000000UL
+#include "util/delay.h"
+
+#define fan_0_EPPROM_adr 0
+#define fan_1_EPPROM_adr 64
+#define fan_2_EPPROM_adr 128
+#define fan_3_EPPROM_adr 192
+#define fan_4_EPPROM_adr 256
+#define fan_5_EPPROM_adr 320
+#define fan_6_EPPROM_adr 384
+#define fan_7_EPPROM_adr 448
+
+// Prototypes
+void my_eeprom_update_byte(uint8_t adr, uint8_t value);
+void my_eeprom_update_char(uint8_t adr, uint16_t value);
+uint8_t my_eeprom_read_byte(uint8_t adr);
+uint16_t my_eeprom_read_char(uint8_t adr);
